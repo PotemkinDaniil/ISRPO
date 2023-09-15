9047df6eddff38dfd099a1dc081726d0bd1a5378
PotemkinDaniil <105436109+PotemkinDaniil@users.noreply.github.com>
2023-09-15 15:21:46 +0400

    This is my second project

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+       
+
+    {
+        int n = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (n % 2 == 0)
+            {
+                label1.ForeColor = Color.Yellow;
+                button1.BackColor = Color.Red;
+                n++;
+            }
+            else if (n % 2 != 0)
+            {
+                label1.ForeColor = Color.Pink;
+                button1.BackColor = Color.DarkBlue;
+                n++;
+            }
+            label1.Top += 5;
+            label1.Text = "Нажмите на кнопочку " + n;
+            
+
+
+        }
+    }
+}
